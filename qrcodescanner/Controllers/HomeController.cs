be4bf3538471942c0a676a3732fd49c46a4d8607
ynssylmz <ynssylmz@gmail.com>
2022-09-16 16:19:55 +0300

    Add project files.

---
+++ qrcodescanner/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Aspose.BarCode.BarCodeRecognition;
+using qrcodescanner.Models;
+
+namespace qrcodescanner.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ResimGoruntule(ResimGoruntuleViewModel model)
+        {
+
+            if (model.Resim.Contains(".jpg") || model.Resim.Contains(".jpeg") || model.Resim.Contains(".JPG") || model.Resim.Contains(".JPEG") || model.Resim.Contains(".png"))
+            {
+                return View(model);
+            }
+           return new JsonResult();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hakkımızda";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "İletişim";
+
+            return View();
+        }
+    }
+}
---
+++ qrcodescanner/Models/ResimGoruntuleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace qrcodescanner.Models
+{
+    public class ResimGoruntuleViewModel
+    {
+        public int ResimId { get; set; }
+        public string Resim { get; set; }
+    }
+}
